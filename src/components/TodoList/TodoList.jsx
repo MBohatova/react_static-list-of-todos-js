796553cc541920fb3d21c0c7be0e78673ea833b4
Mariia Bohatova <maria.bogatova.01@gmail.com>
2025-08-07 18:35:49 +0300

    edited solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -6,16 +6,6 @@ export const TodoList = ({ todos }) => {
     <section className="TodoList">
       {todos.map(todo => (
         <TodoInfo key={todo.id} todo={todo} />
-        // <article
-        //   key={todo.id}
-        //   className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
-        // >
-        //   <h2 className="TodoInfo__title">{todo.title}</h2>
-
-        //   <a className="UserInfo" href={`mailto:${todo.user.email}`}>
-        //     {todo.user.name}
-        //   </a>
-        // </article>
       ))}
     </section>
   );
