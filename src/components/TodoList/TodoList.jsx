6c39cb6e8b02d1007b570f6905c891eac11cfa60
Mariia Bohatova <maria.bogatova.01@gmail.com>
2025-08-07 18:27:19 +0300

    add task solution

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,2 +1,22 @@
 // Add the required props
-export const TodoList = () => <>TodoList markup</>;
+import { TodoInfo } from '../TodoInfo/TodoInfo';
+
+export const TodoList = ({ todos }) => {
+  return (
+    <section className="TodoList">
+      {todos.map(todo => (
+        <TodoInfo key={todo.id} todo={todo} />
+        // <article
+        //   key={todo.id}
+        //   className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+        // >
+        //   <h2 className="TodoInfo__title">{todo.title}</h2>
+
+        //   <a className="UserInfo" href={`mailto:${todo.user.email}`}>
+        //     {todo.user.name}
+        //   </a>
+        // </article>
+      ))}
+    </section>
+  );
+};
